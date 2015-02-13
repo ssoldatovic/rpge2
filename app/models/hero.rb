4fe2b068b4ae96867259ffd8f4a0a9cb9f8edafd
Sinisa Soldatovic <ssoldatovic@yahoo.com>
2015-02-13 23:11:38 +0100

    basic validation

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,5 +1,15 @@
 class Hero < ActiveRecord::Base
+  
   mount_uploader :picture, PictureUploader
+  
   belongs_to :user
   has_many :powers
+  
+  validates :name, :picture, presence: true
+  validates :name, uniqueness: true
+  validates :picture, allow_blank: true, format: {
+    with:    %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+  
 end
--- app/models/power.rb
+++ app/models/power.rb
@@ -2,4 +2,11 @@ class Power < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
   belongs_to :user
   belongs_to :hero
+  validates :name, :value, :picture, presence: true
+  validates :name, uniqueness: true
+  validates :value, numericality: {greater_than_or_equal_to: 0}
+  validates :picture, allow_blank: true, format: {
+    with:    %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
